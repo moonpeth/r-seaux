b373838021c6660ccf861a81921e5d2955eb3f56
Nicolas Forget <forget@polytech.unice.fr>
2014-11-12 10:11:41 +0100

    client-serveur V0

--- ObjetReseau/src/client/Client.java
+++ ObjetReseau/src/client/Client.java
@@ -1,5 +1,49 @@
 package client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class Client {
+
+	public static void main(String[] args) {
+
+		Socket socket;
+		BufferedReader in;
+		PrintWriter out;
+
+		try {
+
+			socket = new Socket("127.0.0.1", 6969);
+			out = new PrintWriter(socket.getOutputStream(),true);
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			Scanner sc = new Scanner(System.in);
+			String line;
+			System.out.print("Client connected at 127.0.0.1:6969");
+			while(true){
+				System.out.print("Ecris un truc : ");
+				line = sc.nextLine();
+				out.println(line);
+				out.flush();
+				System.out.println(in.readLine());
+				if(line.equals("exit")){
+					break;
+				}
+			}
+			sc.close();
+			socket.close();
+
+		}catch (UnknownHostException e) {
+			System.out.println("UnknownHost :");
+			e.printStackTrace();
+		}catch (IOException e) {
+			System.out.println("IOException :");
+			e.printStackTrace();
+		}
+	}
 
 }
--- ObjetReseau/src/serveur/Serveur.java
+++ ObjetReseau/src/serveur/Serveur.java
@@ -1,5 +1,63 @@
 package serveur;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Serveur {
+	private static int DEFAULTPORT = 8888;
+	private ServerSocket socketServer;
+
+	public Serveur(){
+		this(DEFAULTPORT);
+	}
+
+	public Serveur(int port){
+		try{
+			socketServer = new ServerSocket(port);
+		} catch (IOException e) {
+			System.err.println("Server could not listen on port " + port);
+			System.exit(-1);
+		}
+	}
+
+	public void run() {
+		while(true){
+			try {
+				new MultiServerThread(socketServer.accept()).start();
+			} catch (IOException e) {
+				System.err.println("Server could not listen on port " + socketServer.getLocalPort());
+				System.exit(-1);
+			}
+		}
+	}
+
+	class MultiServerThread extends Thread{
+		private Socket socket = null ;
+
+		public MultiServerThread(Socket socket){
+			super("MultiServerThread");
+			this.socket = socket;
+		}
+
+		public void run() {
+			try {
+				//TODO : body
+			} catch (Exception e) {
+				try {
+					System.out.println("Client terminate");
+					socket.close();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	public static void main(String args[]) {
+		System.out.println("Server launch at port 6969");
+		new Serveur(6969).run();
+		System.out.println("Server close");
+	}
 
 }
