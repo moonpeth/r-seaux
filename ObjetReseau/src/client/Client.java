55570e66446c48c7f73cafcb1d45e7836b9cb3be
Nicolas Forget <forget@polytech.unice.fr>
2014-11-12 11:21:00 +0100

    client-serveur V0.1

--- ObjetReseau/src/client/Client.java
+++ ObjetReseau/src/client/Client.java
@@ -3,33 +3,36 @@ package client;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
+import java.io.Serializable;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class Client {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException {
 
 		Socket socket;
-		BufferedReader in;
-		PrintWriter out;
+		ObjectInputStream is;
+		ObjectOutputStream os;
 
 		try {
 
-			socket = new Socket("127.0.0.1", 6969);
-			out = new PrintWriter(socket.getOutputStream(),true);
-			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			socket = new Socket("134.59.214.216", 6969);
+			os = new ObjectOutputStream(socket.getOutputStream());
+			is = new ObjectInputStream(socket.getInputStream());
 			Scanner sc = new Scanner(System.in);
 			String line;
-			System.out.print("Client connected at 127.0.0.1:6969");
+			System.out.println("Client connected at 127.0.0.1:6969");
 			while(true){
 				System.out.print("Ecris un truc : ");
 				line = sc.nextLine();
-				out.println(line);
-				out.flush();
-				System.out.println(in.readLine());
+				os.writeObject((Serializable)line);
+				os.flush();
+				System.out.println(is.readObject());
 				if(line.equals("exit")){
 					break;
 				}
--- ObjetReseau/src/serveur/Serveur.java
+++ ObjetReseau/src/serveur/Serveur.java
@@ -7,6 +7,10 @@ import java.net.Socket;
 public class Serveur {
 	private static int DEFAULTPORT = 8888;
 	private ServerSocket socketServer;
+	private Socket socketClient;
+	private String line;
+	private DataInputStream is;
+	private PrintStream os;
 
 	public Serveur(){
 		this(DEFAULTPORT);
@@ -24,7 +28,14 @@ public class Serveur {
 	public void run() {
 		while(true){
 			try {
-				new MultiServerThread(socketServer.accept()).start();
+				//new MultiServerThread(socketServer.accept()).start();
+				socketClient = socketServer.accept();
+				is = new DataInputStream(socketClient.getInputStream());
+				os = new PrintStream(socketClient.getOutputStream());
+				while(true){
+					line = is.readLine();
+					os.print(line);
+				}
 			} catch (IOException e) {
 				System.err.println("Server could not listen on port " + socketServer.getLocalPort());
 				System.exit(-1);
@@ -32,27 +43,27 @@ public class Serveur {
 		}
 	}
 
-	class MultiServerThread extends Thread{
-		private Socket socket = null ;
-
-		public MultiServerThread(Socket socket){
-			super("MultiServerThread");
-			this.socket = socket;
-		}
-
-		public void run() {
-			try {
-				//TODO : body
-			} catch (Exception e) {
-				try {
-					System.out.println("Client terminate");
-					socket.close();
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
-			}
-		}
-	}
+//	class MultiServerThread extends Thread{
+//		private Socket socket = null ;
+//
+//		public MultiServerThread(Socket socket){
+//			super("MultiServerThread");
+//			this.socket = socket;
+//		}
+//
+//		public void run() {
+//			try {
+//				//TODO : body
+//			} catch (Exception e) {
+//				try {
+//					System.out.println("Client terminate");
+//					socket.close();
+//				} catch (IOException e1) {
+//					e1.printStackTrace();
+//				}
+//			}
+//		}
+//	}
 	
 	public static void main(String args[]) {
 		System.out.println("Server launch at port 6969");
