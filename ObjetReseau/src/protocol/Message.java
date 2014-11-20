772c9e373b24440280e2b410ea43f7fe25c54f5d
Nicolas Forget <forget@polytech.unice.fr>
2014-11-20 10:56:35 +0100

    refactoring

--- ObjetReseau/src/protocol/Message.java
+++ ObjetReseau/src/protocol/Message.java
@@ -1,6 +1,5 @@
 package protocol;
 
-import java.awt.List;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -9,23 +8,27 @@ import java.util.Scanner;
 public class Message implements Serializable{
 
 	private static final long serialVersionUID = -6503571106963452874L;
-	Type requete;
-	ArrayList<String> arg = new ArrayList<>();;
-	//constructer for client part
+	private Type requete;
+	private ArrayList<String> args = new ArrayList<>();
+
 	public Message(Scanner commande){
 		while (commande.hasNext()) {
 			String fromUser = commande.nextLine();
-			String[] parts = fromUser.split(",");		     
+			String[] parts = fromUser.split(" ");		     
 			this.requete=Type.getType(parts[0]);
-			this.arg.add(parts[1]);
-			this.arg.add(parts[2]);
-			// this(Type.getType(parts[0]),parts[1],parts[2]);
+			this.args.add(parts[1]);
+			this.args.add(parts[2]);
 			break;
 		}
 	}
 	
+	public Message(String req, ArrayList<String> arg){	     
+		this.requete = Type.getType(req);
+		this.args = arg;
+	}
+
 	public String toString(){
-		String ret="REQ : "+requete+" "+arg;
+		String ret="REQ : "+requete+" "+args;
 		return ret;
 	}
 
--- ObjetReseau/src/serveur/Serveur.java
+++ ObjetReseau/src/serveur/Serveur.java
@@ -10,7 +10,13 @@ import java.net.Socket;
 import protocol.Message;
 
 public class Serveur {
-	static final int port = 6356;
+
+	private static String DEFAULT_IP = "127.0.0.1";
+	private static int DEFAULT_PORT = 8888;
+	
+	private static ServerSocket serverSocket;
+	private static ObjectInputStream in;
+	private static ObjectOutputStream out;
 
 	public static void main(String[] args) throws IOException {
 
@@ -21,41 +27,23 @@ public class Serveur {
 		//
 		// int portNumber = Integer.parseInt(args[0]);
 		System.out.println("serveur launch");
-		try (ServerSocket serverSocket = new ServerSocket(port);
-				Socket clientSocket = serverSocket.accept();
-				ObjectOutputStream out = new ObjectOutputStream(
-						clientSocket.getOutputStream());
-				ObjectInputStream in = new ObjectInputStream(
-						clientSocket.getInputStream());) {
-			
-             Message messageObject = (Message)in.readObject();
-//             Handmessage handmessage = new Handmessage(messageObject);
-           System.out.println(messageObject.toString());
-             
-//			Object inputLine = 
-//			Object outputLine =
-//
-//			// Initiate conversation with client
-//			Protocol protocol = new Protocol();
-//			outputLine = protocol.processInput(null);
-//			out.writeObject(outputLine);
-//
-//			while ((inputLine = in.readObject()) != null) {
-//				outputLine = protocol.processInput(inputLine);
-//				out.writeObject(outputLine);
-//				if (outputLine.equals("Bye."))
-//					break;
-			
-		} catch(IOException e) {
-			System.out
-					.println("Exception caught when trying to listen on port "
-							+ port + " or listening for a connection");
-			System.out.println(e.getMessage());
-		} catch(ClassNotFoundException e) {
-			System.err.println("ClassNotFoundException"
-					+ InetAddress.getLocalHost());
-			System.exit(1);
+		
+		try {
+			Message messageObject;
+			serverSocket = new ServerSocket(DEFAULT_PORT);
+			Socket clientSocket = serverSocket.accept();
+			out = new ObjectOutputStream(clientSocket.getOutputStream());
+			in = new ObjectInputStream(clientSocket.getInputStream());
+			messageObject = (Message)in.readObject();
+			//Handmessage handmessage = new Handmessage(messageObject);
+			System.out.println(messageObject.toString());
+			serverSocket.close();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-	}}
+		
+	}
+}
 
 
