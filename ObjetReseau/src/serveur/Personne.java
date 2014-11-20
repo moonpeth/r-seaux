5b5cdf201a2a933432d212b7bd0d28f9dbc0c571
Nicolas Forget <forget@polytech.unice.fr>
2014-11-20 13:55:34 +0100

    exceptions - handmessage.add

--- ObjetReseau/src/serveur/Personne.java
+++ ObjetReseau/src/serveur/Personne.java
@@ -3,6 +3,7 @@ package serveur;
 import java.util.ArrayList;
 
 public class Personne {
+	
 	private String nom;
 	private ArrayList<String> surnoms = new ArrayList<String>();
 
@@ -21,5 +22,36 @@ public class Personne {
 	}
 	public ArrayList<String> getsurnoms(){
 		return surnoms;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nom == null) ? 0 : nom.hashCode());
+		result = prime * result + ((surnoms == null) ? 0 : surnoms.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Personne other = (Personne) obj;
+		if (nom == null) {
+			if (other.nom != null)
+				return false;
+		} else if (!nom.equals(other.nom))
+			return false;
+		if (surnoms == null) {
+			if (other.surnoms != null)
+				return false;
+		} else if (!surnoms.equals(other.surnoms))
+			return false;
+		return true;
 	}
 }
