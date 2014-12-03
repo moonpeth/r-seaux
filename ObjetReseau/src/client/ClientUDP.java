42547cd979320678c98338ca2f71c13e4a96097f
Nicolas Forget <forget@polytech.unice.fr>
2014-12-03 10:25:20 +0100

    UDP

--- ObjetReseau/src/client/ClientUDP.java
+++ ObjetReseau/src/client/ClientUDP.java
@@ -1,38 +1,119 @@
 package client;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
-
-// Object de communication
 public class ClientUDP {
+
+	//attributes
+	private static byte[] buffer;
+	private static String hostname;
+	private static int hostport;
+	private static String message;
+
+	// Constructor 
+	public ClientUDP(String adrIp, int port){   	
+		hostname = adrIp;
+		hostport = port;
+		message = "";
+		System.out.println("client initialised");
+	}
 
 	//main test
 	public static void main(String[] args) {
 
-		try {
-			BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-			DatagramSocket clientSocket;
-			clientSocket = new DatagramSocket();
-			InetAddress IPAddress = InetAddress.getByName("localhost");
-			byte[] sendData = new byte[1024];
-			byte[] receiveData = new byte[1024];
-			String sentence = inFromUser.readLine();
-			sendData = sentence.getBytes();
-			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-			clientSocket.send(sendPacket);
-			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-			clientSocket.receive(receivePacket);
-			String modifiedSentence = new String(receivePacket.getData());
-			System.out.println("FROM SERVER:" + modifiedSentence);
-			clientSocket.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		ClientUDP client = new ClientUDP("localhost", 8888);
+
+		//add , by A
+		System.out.println("\n----------A : add person-------------------------------");
+		client.setMessage("add, Nicolassssssssssss, Nicos");
+		buffer = client.getMessage().getBytes();
+		envoi(client.getHostname(),client.getHostport(),buffer);
+
+		//add another
+		System.out.println("\n----------A : add another two persons------------------");
+		client.setMessage("add, Nicolas, Nico");
+		buffer = client.getMessage().getBytes();
+		envoi(client.getHostname(),client.getHostport(),buffer);
+
+		//add another
+		client.setMessage("add, Jimmy, Jim");
+		buffer = client.getMessage().getBytes();
+		envoi(client.getHostname(),client.getHostport(),buffer);
+
+		//get Jim
+		System.out.println("\n----------A : get name ,get all,add nickname-----------");
+		client.setMessage("get, Jim");
+		buffer = client.getMessage().getBytes();
+		envoi(client.getHostname(),client.getHostport(),buffer);
+
+		//get all   
+		client.setMessage("get");
+		buffer = client.getMessage().getBytes();
+		envoi(client.getHostname(),client.getHostport(),buffer);
+
+		client.setMessage("adds, Nicoco, Ninico");
+		buffer = client.getMessage().getBytes();
+		envoi(client.getHostname(),client.getHostport(),buffer);
+
+		client.setMessage("get, Ninico");
+		buffer = client.getMessage().getBytes();
+		envoi(client.getHostname(),client.getHostport(),buffer);
+
+		System.out.println("\n----------A : EXIT--------------------------------------");
+		//exit
+		client.setMessage("exit");
+		buffer = client.getMessage().getBytes();
+		envoi(client.getHostname(),client.getHostport(),buffer);
+	}
+
+	public String getHostname() {
+		return hostname;
+	}
+
+	public void setHostname(String hostname) {
+		ClientUDP.hostname = hostname;
+	}
+
+	public int getHostport() {
+		return hostport;
+	}
+
+	public void setHostport(int hostport) {
+		ClientUDP.hostport = hostport;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		ClientUDP.message = message;
+	}
+
+	public static void envoi (String hostname, int hostport, byte[] buffer){
+		try{
+			InetAddress host_address = InetAddress.getByName(hostname);
+			@SuppressWarnings("resource")
+			DatagramSocket socket = new DatagramSocket();
+			DatagramPacket paquet = new DatagramPacket(buffer, buffer.length,host_address,hostport);
+			socket.send(paquet);
+		}catch(Exception e){
+			System.err.println("Erreur: "+e);
+		}
+
+	}
+
+	static public void reception (int hostport){
+		try{
+			@SuppressWarnings("resource")
+			DatagramSocket socket = new DatagramSocket(hostport);
+			DatagramPacket paquet = new DatagramPacket(new byte[512],512);
+			socket.receive(paquet);
+			System.out.println("Paquet recu : "+ new String(paquet.getData()));
+		}catch(Exception e){
+			System.err.println("Erreur: "+e);
 		}
 	}
 
--- ObjetReseau/src/serveur/ServeurUDP.java
+++ ObjetReseau/src/serveur/ServeurUDP.java
@@ -7,23 +7,70 @@ import java.net.InetAddress;
 
 public class ServeurUDP {
 
-	public static void main(String[] args) throws IOException {
-		@SuppressWarnings("resource")
-		DatagramSocket serverSocket = new DatagramSocket(9876);
-		byte[] receiveData = new byte[1024];
-		byte[] sendData = new byte[1024];
-		while(true){
-			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-			serverSocket.receive(receivePacket);
-			String sentence = new String( receivePacket.getData());
-			System.out.println("RECEIVED: " + sentence);
-			InetAddress IPAddress = receivePacket.getAddress();
-			int port = receivePacket.getPort();
-			String capitalizedSentence = sentence.toUpperCase();
-			sendData = capitalizedSentence.getBytes();
-			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
-			serverSocket.send(sendPacket);
+	static int hostport = 8532;
+	static String hostname = "localhost";
+	static byte[] buffer ;
+
+
+	public static void envoi (String hostname, int hostport, byte[] buffer){
+		try{
+			InetAddress host_address = InetAddress.getByName(hostname);
+			@SuppressWarnings("resource")
+			DatagramSocket socket = new DatagramSocket();
+			DatagramPacket paquet = new DatagramPacket(buffer, buffer.length,host_address,hostport);
+			socket.send(paquet);
+		}catch(Exception e){
+			System.err.println("Erreur: "+e);
 		}
+
 	}
 
+	static public DatagramPacket reception (int hostport){
+		try{
+			@SuppressWarnings("resource")
+			DatagramSocket socket = new DatagramSocket(hostport);
+			// while(true){
+			DatagramPacket paquet = new DatagramPacket(new byte[512],512);
+			socket.receive(paquet);
+			System.out.println("Addresse Serveur:"+ paquet.getAddress());
+			System.out.println("Donnees recues :"+ new String(paquet.getData()));
+			return paquet;
+			//}
+		}catch(Exception e){
+			System.err.println("Erreur: "+e);
+			return null;
+		}
+
+	}
+
+	/*    static public void reception (int hostport){
+            try{
+		DatagramSocket socket = new DatagramSocket(hostport);
+                    while(true){
+                    DatagramPacket paquet = new DatagramPacket(new byte[512],512);
+                    socket.receive(paquet);
+                    System.out.println("Contenu du paquet Addresse Serveur:"+ paquet.getAddress());
+                    System.out.println("Contenu du paquet Donnees:"+ new String(paquet.getData()));
+                    }
+                  }catch(Exception e){
+		System.err.println("Erreur: "+e);
+                 }
+
+    } */ 
+
+
+	public static void main(String[] args) throws IOException{
+		while(true){
+			//Reception du paquet
+			DatagramPacket paquet = reception(hostport); //pb:recupration du paquet pour le renvoyer au clientle serveur s arrete apres la first reception
+
+			//reception(hostport);
+
+			//envoi du paquet
+			hostname = paquet.getAddress().getHostAddress();
+			hostport = paquet.getPort();
+			buffer = paquet.getData();
+			envoi(hostname,hostport,buffer);
+		}
+	}
 }
